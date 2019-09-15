2e4333a1695a78c750cf58f7c4054820a2b1d835
Piotr Osoba <piotrosoba@outlook.com>
2019-09-15 11:11:28 +0200

    auth logic, make fetchWithToken

--- src/state/auth.js
+++ src/state/auth.js
@@ -1,10 +1,52 @@
 import axios from 'axios'
 import jwt from 'jsonwebtoken'
 import { fullScreenCircural } from './fullScreenCircural'
+import { addSnackbarActionCreator } from './snackbars'
 
 import { REGISTER_URL, LOG_IN_URL, RESET_PASSWORD, REFRESH_TOKEN_URL } from '../consts/firebase'
 
 const SAVE_DECODED_TOKEN = 'auth/SAVE_DECODED_TOKEN'
+const LOG_OUT = 'auth/LOG_OUT'
+
+export const fetchWithTokenAndProgress = (url, method = 'get', data = {}) => (dispatch) => {
+  dispatch(fullScreenCircural.add())
+  return dispatch(fetchWithToken(url, method = 'get', data = {}))
+    .finally((r) => {
+      dispatch(fullScreenCircural.remove())
+      return r
+    })
+}
+
+export const fetchWithToken = (url, method = 'get', data = {}) => (dispatch, getState) => {
+  const getUrlWithToken = () => {
+    const auth = getState().auth
+    if (auth.idToken)
+      return url.includes('?') ? url + '&auth=' + auth.idToken : url + '?auth=' + auth.idToken
+
+    return url
+  }
+
+  return axios({
+    url: getUrlWithToken(),
+    method,
+    data
+  })
+    .catch(r => {
+      const refreshToken = getState().auth.refreshToken || localStorage.getItem('refreshToken')
+      if (r.response.statusText === 'Unauthorized') { }
+      return dispatch(useRefreshToken(refreshToken))
+        .catch(() => {
+          dispatch(logOut())
+          dispatch(addSnackbarActionCreator('Your session expired, log in again', 'red'))
+          return Promise.reject(r)
+        })
+        .then(() => axios({
+          url: getUrlWithToken(),
+          method,
+          data
+        }))
+    })
+}
 
 export const registerUserAsyncActionCreator = (email, password) => (dispatch) => {
   return axios({
@@ -65,22 +107,34 @@ export const checkIsUserLoggedIn = () => (dispatch) => {
     dispatch(saveAndDecodeToken(idToken, refreshToken))
   } else if (refreshToken) {
     dispatch(fullScreenCircural.add())
-    axios({
-      method: 'post',
-      url: REFRESH_TOKEN_URL,
-      data: {
-        grant_type: 'refresh_token',
-        refresh_token: refreshToken
-      }
-    })
-      .then(response => {
-        if (response.data)
-          dispatch(saveAndDecodeToken(response.data.id_token, response.data.refresh_token))
-      })
-      .catch(r => r)
+    dispatch(useRefreshToken(refreshToken))
       .finally(() => dispatch(fullScreenCircural.remove()))
   }
 }
+
+const useRefreshToken = refreshToken => dispatch => {
+  return axios({
+    method: 'post',
+    url: REFRESH_TOKEN_URL,
+    data: {
+      grant_type: 'refresh_token',
+      refresh_token: refreshToken
+    }
+  })
+    .then(response => {
+      if (response.data)
+        dispatch(saveAndDecodeToken(response.data.id_token, response.data.refresh_token))
+      return response
+    })
+}
+
+export const logOut = () => dispatch => {
+  localStorage.removeItem('refreshToken')
+  localStorage.removeItem('idToken')
+  dispatch(logOutActionCreator())
+}
+
+const logOutActionCreator = () => ({ type: LOG_OUT })
 
 const saveAndDecodeToken = (idToken, refreshToken) => (dispatch) => {
   localStorage.setItem('idToken', idToken)
@@ -109,9 +163,21 @@ export default (state = initialState, action) => {
         ...state,
         isLogged: true,
         idToken: action.idToken,
-        refreshToken: action.idToken,
+        refreshToken: action.refreshToken,
         ...action.decodedToken
       }
+    case LOG_OUT: {
+      return {
+        ...state,
+        isLogged: false
+      }
+    }
+    // case 'brokeToken':
+    //   return {
+    //     ...state,
+    //     idToken: null,
+    //     refreshToken: null
+    //   }
     default:
       return state
   }
